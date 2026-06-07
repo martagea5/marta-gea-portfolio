7a6dcb03ef184a2e123bd5c5b6b961dc10e651af
carmen <cpascual@laberit.com>
2026-06-07 19:56:56 +0200

    POPE DELI: planos al inicio y sin barra inferior
    Reordena los planos técnicos debajo de la portada y recorta la barra de cajetín de los cuatro planos.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -75,6 +75,10 @@ export const projects: Project[] = [
     meta: "Interiorismo hospitality · Valencia · Salamandra Interiorismo",
     images: [
       { src: "/img/projects/popedeli-1.jpg", caption: "Fachada POPE DELI Valencia" },
+      { src: "/img/projects/popedeli-13.jpg", caption: "Planta propuesta" },
+      { src: "/img/projects/popedeli-14.jpg", caption: "Planta de pavimentos" },
+      { src: "/img/projects/popedeli-15.jpg", caption: "Planta de revestimientos" },
+      { src: "/img/projects/popedeli-16.jpg", caption: "Sección constructiva" },
       { src: "/img/projects/popedeli-2.jpg", caption: "Zona de barra alta" },
       { src: "/img/projects/popedeli-3.jpg", caption: "Sala principal" },
       { src: "/img/projects/popedeli-4.jpg", caption: "Galería de cuadros" },
@@ -86,10 +90,6 @@ export const projects: Project[] = [
       { src: "/img/projects/popedeli-10.jpg", caption: "Detalle lavabo" },
       { src: "/img/projects/popedeli-11.jpg", caption: "Estantería de barra" },
       { src: "/img/projects/popedeli-12.jpg", caption: "Sala con mesa redonda" },
-      { src: "/img/projects/popedeli-13.jpg", caption: "Planta propuesta" },
-      { src: "/img/projects/popedeli-14.jpg", caption: "Planta de pavimentos" },
-      { src: "/img/projects/popedeli-15.jpg", caption: "Planta de revestimientos" },
-      { src: "/img/projects/popedeli-16.jpg", caption: "Sección constructiva" },
     ],
   },
   {
